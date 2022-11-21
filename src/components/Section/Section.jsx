d1034857d9301debccc1ca7ff43f8f4091cdb5bf
Vitaliy <103895059+vitaliy197811@users.noreply.github.com>
2022-11-21 21:06:11 +0200

    finish 1

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,7 +4,7 @@ import css from './Section.module.css';
 
 const Section = ({ title, children }) => (
     <section className={css.section}>
-        {title}
+        <h2>{title}</h2>
         {children}
     </section>
 );
